b0d69cf30e79f23671a124d7f066b29a7c4cdec1
Lucas Gati Borges <lgati@insider.com>
2025-04-27 13:20:20 -0300

    adding id

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,10 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   use: {
-    screenshot: 'only-on-failure', // Take screenshots on failures
-    video: 'retain-on-failure',    // Keep videos for failed tests
-    trace: 'on-first-retry',        // Record trace on first retry (nice bonus)
+    screenshot: "only-on-failure", // Take screenshots on failures
+    video: "retain-on-failure", // Keep videos for failed tests
+    trace: "on-first-retry", // Record trace on first retry (nice bonus)
   },
-  reporter: [
-    ['list'],
-    ['@currents/playwright', { projectId: 'EILla1' }]
-  ]
+  reporter: [["list"], ["@currents/playwright", { projectId: "EILla1" }]],
 });
