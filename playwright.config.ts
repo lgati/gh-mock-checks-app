1805252084453d940529f999eba2450c964a3410
Lucas Gati Borges <lgati@insider.com>
2025-04-27 11:59:56 -0300

    upload artifacts

---
+++ playwright.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  use: {
+    screenshot: 'only-on-failure', // Take screenshots on failures
+    video: 'retain-on-failure',    // Keep videos for failed tests
+    trace: 'on-first-retry',        // Record trace on first retry (nice bonus)
+  },
+});
