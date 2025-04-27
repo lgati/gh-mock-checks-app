d5878d916c8ec5660540e3dbfc7a082f00e8a22b
Lucas Gati Borges <lgati@insider.com>
2025-04-27 15:27:24 -0300

    fixing workflow

--- tests/pass.spec.ts
+++ tests/pass.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { argosScreenshot } from "@argos-ci/playwright";
 
 test('Simple passing test 1', async ({ page }) => {
   await page.goto('https://playwright.dev/');
@@ -8,4 +9,5 @@ test('Simple passing test 1', async ({ page }) => {
 test('Simple passing test 2', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   await expect(page).toHaveTitle(/Playwright/);
+  await argosScreenshot(page, "homepage");
 });
